3e69e4c800da1656a3db47156a8371146b42288c
Frédéric Simonis <simonisfrederic@gmail.com>
2026-05-08 14:30:44 +0200

    Extend run file

--- run.sh
+++ run.sh
@@ -2,15 +2,24 @@
 
 set -ex
 
+# setup venv
 python3 -m venv venv
 . venv/bin/activate
 pip install -r requirements.txt
 
+# check if all commands are available
 hash blockMesh
 hash simpleFoam
 hash micro-manager-precice
 
+# check if the micro simulation is importable
 python3 -c "import micro.micro"
+
+# cleanup
+
+rm -fr precice-run
+
+# run
 
 ( cd macro && python3 macro.py ) &
 
